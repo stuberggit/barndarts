9e382e788a22344c12a6ae6fa5581657c40cc3f1
stuberggit <stuberg+github@gmail.com>
2026-04-17 07:24:20 -0500

    Update GameScreen.js

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,20 +1,25 @@
 import { store } from "../core/store.js";
 import { renderApp } from "../core/router.js";
 
+import golfdarts from "../games/golfdarts/index.js";
+
+const gameMap = {
+  "GolfDarts": golfdarts
+};
+
 export function renderGame(container) {
+  const game = gameMap[store.selectedGame];
+
   container.innerHTML = `
     <h1>${store.selectedGame}</h1>
-
-    <div id="players"></div>
-
+    <div id="gameArea"></div>
     <div class="button" id="end">End Game</div>
   `;
 
-  const playersDiv = document.getElementById("players");
+  const gameArea = document.getElementById("gameArea");
 
-  playersDiv.innerHTML = store.players
-    .map(p => `<div>${p}: 0</div>`)
-    .join("");
+  game.start(store.players);
+  game.render(gameArea);
 
   document.getElementById("end").onclick = () => {
     store.screen = "HOME";
