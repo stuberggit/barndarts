0d08bc289872cbee54ee5218acbe35ea3b2cd84e
stuberggit <stuberg+github@gmail.com>
2026-04-20 13:44:34 -0500

    Update index.js

--- games/hammer-cricket/index.js
+++ games/hammer-cricket/index.js
@@ -1,1 +1,15 @@
+import { initGame } from "./logic.js";
+import { renderUI } from "./ui.js";
 
+export default {
+  id: "hammer-cricket",
+  name: "Hammer Cricket",
+
+  start(players) {
+    initGame(players);
+  },
+
+  render(container) {
+    renderUI(container);
+  }
+};
