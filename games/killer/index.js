56bcc9dd54100937b632307e01a7b61c8b6b50d7
stuberggit <stuberg+github@gmail.com>
2026-04-22 10:58:29 -0500

    Update index.js

--- games/killer/index.js
+++ games/killer/index.js
@@ -1,1 +1,15 @@
+import { initGame } from "./logic.js";
+import { renderUI } from "./ui.js";
 
+export default {
+  id: "killer",
+  name: "Killer",
+
+  start(players) {
+    initGame(players);
+  },
+
+  render(container) {
+    renderUI(container);
+  }
+};
