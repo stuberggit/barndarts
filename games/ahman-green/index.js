10f5169915988a0c472186b6cf9d9dde83ef6c3d
stuberggit <stuberg+github@gmail.com>
2026-04-21 21:40:24 -0500

    Update index.js

--- games/ahman-green/index.js
+++ games/ahman-green/index.js
@@ -1,1 +1,15 @@
+import { initGame } from "./logic.js";
+import { renderUI } from "./ui.js";
 
+export default {
+  id: "ahman-green",
+  name: "Ahman Green",
+
+  start(players) {
+    initGame(players);
+  },
+
+  render(container) {
+    renderUI(container);
+  }
+};
