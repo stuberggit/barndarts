7b0c060aa95004ef72af44a4e2a9674d6c16532c
stuberggit <stuberg+github@gmail.com>
2026-04-23 14:08:12 -0500

    Update index.js

--- games/survivor-301/index.js
+++ games/survivor-301/index.js
@@ -1,1 +1,15 @@
+import { initGame } from "./logic.js";
+import { renderUI } from "./ui.js";
 
+export default {
+  id: "survivor301",
+  name: "Survivor 301",
+
+  start(players) {
+    initGame(players);
+  },
+
+  render(container) {
+    renderUI(container);
+  }
+};
