a75244b0bf5aa11f438010c6ac331e6c8fa59d2e
stuberggit <stuberg+github@gmail.com>
2026-04-24 15:03:47 -0500

    Update router.js

--- core/router.js
+++ core/router.js
@@ -3,6 +3,11 @@ import { renderHome } from "../screens/HomeScreen.js";
 import { renderCategory } from "../screens/CategoryScreen.js";
 import { renderSetup } from "../screens/SetupScreen.js";
 import { renderGame } from "../screens/GameScreen.js";
+import { renderHistory } from "../screens/HistoryScreen.js";
+
+if (store.screen === "HISTORY") {
+  renderHistory(container);
+}
 
 export function renderApp() {
   const app = document.getElementById("app");
