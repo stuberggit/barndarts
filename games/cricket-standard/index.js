0a07ba9dd6e63e4400bbeadc37daa756327a4a32
stuberggit <stuberg+github@gmail.com>
2026-04-27 12:49:27 -0500

    Update index.js

--- games/cricket-standard/index.js
+++ games/cricket-standard/index.js
@@ -1,1 +1,15 @@
+import { initGame } from "./logic.js";
+import { renderUI } from "./ui.js";
 
+export default {
+  id: "cricket-standard",
+  name: "Standard Cricket",
+
+  start(players) {
+    initGame(players);
+  },
+
+  render(container) {
+    renderUI(container);
+  }
+};
