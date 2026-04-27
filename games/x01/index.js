07f0abb0dad8a73832953bb69c2023d9e2beb1d3
stuberggit <stuberg+github@gmail.com>
2026-04-27 13:05:02 -0500

    Update index.js

--- games/x01/index.js
+++ games/x01/index.js
@@ -1,1 +1,13 @@
+import { initGame } from "./logic.js";
+import { renderUI } from "./ui.js";
 
+export default {
+  id: "x01",
+  name: "X01",
+  start(players) {
+    initGame(players);
+  },
+  render(container) {
+    renderUI(container);
+  }
+};
