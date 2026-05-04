add32b05ee3957179cb9087b71476018b97439cc
stuberggit <stuberg+github@gmail.com>
2026-05-04 12:25:05 -0500

    Update GameScreen.js

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -24,7 +24,7 @@ const gameMap = {
   "cricket-standard": CricketStandard,
   "cricket-no-score": CricketNoScore,
   "gotcha": Gotcha,
-  "BattleDarts": BattleDarts
+  "battleDarts": BattleDarts
 };
 
 const gameDisplayNames = {
