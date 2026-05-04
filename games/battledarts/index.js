9c522911f042622dfe84b83cf573c5caa84b137e
stuberggit <stuberg+github@gmail.com>
2026-05-04 12:21:29 -0500

    Update index.js

--- games/battledarts/index.js
+++ games/battledarts/index.js
@@ -1,1 +1,15 @@
+import { initGame } from "./logic.js";
+import { renderUI } from "./ui.js";
 
+export default {
+  id: "battledarts",
+  name: "BattleDarts",
+
+  start(players) {
+    initGame(players);
+  },
+
+  render(container) {
+    renderUI(container);
+  }
+};
