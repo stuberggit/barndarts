790d2ca2e3483ef73c14a7444b339d2b736c524e
stuberggit <stuberg+github@gmail.com>
2026-05-05 10:01:31 -0500

    Update ui.js

--- games/battledarts/ui.js
+++ games/battledarts/ui.js
@@ -39,14 +39,6 @@ function getKnownTargets(team) {
   });
 }
 
-function getKnownMisses(team) {
-  return Object.values(team.missIntel || {}).sort((a, b) => {
-    if (a.target === 25) return 1;
-    if (b.target === 25) return -1;
-    return a.target - b.target;
-  });
-}
-
 function getFleetIntelForShip(activeTeam, targetTeamIndex, target) {
   return activeTeam?.fleetIntel?.[`${targetTeamIndex}:${target}`] || null;
 }
