bd75a5236a997a8a9fd13605403cdd226066c503
stuberggit <stuberg+github@gmail.com>
2026-05-08 14:16:41 -0500

    Update ui.js

--- games/ahman-green/ui.js
+++ games/ahman-green/ui.js
@@ -792,22 +792,6 @@ function renderEnd(container, state) {
       border:2px solid #facc15;
       animation:agGlow 2.8s infinite ease-in-out;
     ">
-      <div style="
-        position:absolute;
-        top:10px;
-        left:-24px;
-        right:-24px;
-        display:flex;
-        justify-content:space-between;
-        pointer-events:none;
-        font-size:26px;
-        opacity:0.15;
-      ">
-        <span style="animation:moneyFloat 2.2s infinite ease-in-out;">🖤</span>
-        <span style="animation:moneyFloat 2.6s infinite ease-in-out;">⚪</span>
-        <span style="animation:moneyFloat 2.1s infinite ease-in-out;">🟢</span>
-        <span style="animation:moneyFloat 2.8s infinite ease-in-out;">🔴</span>
-      </div>
 
       <div style="
         text-align:center;
